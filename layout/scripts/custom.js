174b5bd3c0526287b205e63981247e2789c8136f
theonealf@gmail.com <theonealf@gmail.com>
2014-08-26 11:35:42 +0200

    14-08-26:11:35
    Mediafrågorna klara. skalar fint i alla enheter. ej testat med text än

--- layout/scripts/custom.js
+++ layout/scripts/custom.js
@@ -1,63 +1,63 @@
 jQuery(document).ready(function ($) {
-    $('.isotope').isotope({
-        itemSelector: '.item',
-        masonry: {
-            columnWidth: 242
-        }
+   $('#flexmenu').slicknav({
+       label: '',
+       prependTo: '#flexmenubox'
     });
-    $("#logon").css('position', 'relative');
-    $("#logon").css("z-index", "10000");
-    /* Alert Messages */
+    //$('.isotope').isotope({
+    //    itemSelector: '.item',
+    //    masonry: {
+    //        columnWidth: 242
+    //    }
+    //});
+    //$("#logon").css('position', 'relative');
+    //$("#logon").css("z-index", "10000");
+    ///* Alert Messages */
 	
-    $(".alert-msg .close").click(function () {
-        $(this).parent().animate({
-            "opacity": "0"
-        }, 400).slideUp(400);
-        return false;
-    });
+    //$(".alert-msg .close").click(function () {
+    //    $(this).parent().animate({
+    //        "opacity": "0"
+    //    }, 400).slideUp(400);
+    //    return false;
+    //});
 
-    /* Accordions */
+    ///* Accordions */
 
-    $(".accordion-title").click(function () {
-        $(".accordion-title").removeClass("active");
-        $(".accordion-content").slideUp("normal");
-        if ($(this).next().is(":hidden") == true) {
-            $(this).addClass("active");
-            $(this).next().slideDown("normal");
-        }
-    });
-    $(".accordion-content").hide();
+    //$(".accordion-title").click(function () {
+    //    $(".accordion-title").removeClass("active");
+    //    $(".accordion-content").slideUp("normal");
+    //    if ($(this).next().is(":hidden") == true) {
+    //        $(this).addClass("active");
+    //        $(this).next().slideDown("normal");
+    //    }
+    //});
+    //$(".accordion-content").hide();
 
-    /* Toggles */
+    ///* Toggles */
 
-    $(".toggle-title").click(function () {
-        $(this).toggleClass("active").next().slideToggle("fast");
-        return false;
-    });
+    //$(".toggle-title").click(function () {
+    //    $(this).toggleClass("active").next().slideToggle("fast");
+    //    return false;
+    //});
 
-    /* Tabs */
+    
 
-//    $(".tab-wrapper").tabs({
-//        event: "click"
-//    })
+    //$.fn.flexVerticalCenter = function (onAttribute) {
+    //    return this.each(function () {
+    //        var $this = $(this);
+    //        var attribute = onAttribute || 'margin-top';
+    //        var resizer = function () {
+    //            $this.css(
+    //            attribute, (($this.parent().height() - $this.height()) / 2));
+    //        };
+    //        resizer();
+    //        $(window).resize(resizer);
+    //    });
+    //};
 
-    /* Vertically Centre Text On Images */
+    //// To run the function:
+    //$('.viewit').flexVerticalCenter();
 
-    $.fn.flexVerticalCenter = function (onAttribute) {
-        return this.each(function () {
-            var $this = $(this);
-            var attribute = onAttribute || 'margin-top';
-            var resizer = function () {
-                $this.css(
-                attribute, (($this.parent().height() - $this.height()) / 2));
-            };
-            resizer();
-            $(window).resize(resizer);
-        });
-    };
+    
+   
 
-    // To run the function:
-    $('.viewit').flexVerticalCenter();
-
-  
 });
